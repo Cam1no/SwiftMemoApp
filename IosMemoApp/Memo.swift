78c14aad3a781267ee0637c0da1422b6e41597d3
kinjoyuji <kinjoyuji@gmail.com>
2017-06-17 22:10:39 +0900

    ディレクトリ構成を変更しModelオブジェクトを生成

---
+++ IosMemoApp/Memo.swift
@@ -0,0 +1,13 @@
+//
+//  Memo.swift
+//  IosMemoApp
+//
+//  Created by 金城裕治 on 2017/06/17.
+//  Copyright © 2017年 Yuji. All rights reserved.
+//
+
+import UIKit
+
+class Memo: NSObject {
+
+}
---
+++ IosMemoApp/StockMemos.swift
@@ -0,0 +1,13 @@
+//
+//  StockMemos.swift
+//  IosMemoApp
+//
+//  Created by 金城裕治 on 2017/06/17.
+//  Copyright © 2017年 Yuji. All rights reserved.
+//
+
+import UIKit
+
+class StockMemos: NSObject {
+
+}
